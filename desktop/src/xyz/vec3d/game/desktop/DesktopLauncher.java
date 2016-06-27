ba0ad7f45afd21b055b3e1e20adb208c4aec2b5f
darakelian <dha42@drexel.edu>
2016-06-27 16:42:37 -0400

    Initial commit
    This is the initial commit consisting of skeleton code for desktop and android builds.

---
+++ desktop/src/xyz/vec3d/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package xyz.vec3d.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import xyz.vec3d.game.PocketRogue;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PocketRogue(), config);
+	}
+}
