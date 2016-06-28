ae5e94f1665c48922067cd64118416d35e97a1d2
Daron Arakelian <dha42@drexel.edu>
2016-06-28 17:27:52 -0400

    Started working on implementing screen based application.

--- core/src/xyz/vec3d/game/PocketRogue.java
+++ core/src/xyz/vec3d/game/PocketRogue.java
@@ -1,27 +1,27 @@
 package xyz.vec3d.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class PocketRogue extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
+/**
+ * Created by darakelian on 6/27/2016.
+ */
+public class PocketRogue extends Game {
+
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new MenuScreen());
 	}
 
-	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		//Gdx.gl.glClearColor(1, 0, 0, 1);
+		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//batch.begin();
+		//batch.draw(img, 0, 0);
+		//batch.end();
+		super.render();
 	}
 }
