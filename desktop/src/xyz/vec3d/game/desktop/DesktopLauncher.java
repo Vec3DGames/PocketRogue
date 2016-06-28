67ab11356adb6ee3b3341cde9a726b60d6f9711c
Daron Arakelian <daronarakelian@gmail.com>
2016-06-28 08:43:40 -0400

    android studio file commit.

--- desktop/src/xyz/vec3d/game/desktop/DesktopLauncher.java
+++ desktop/src/xyz/vec3d/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import xyz.vec3d.game.PocketRogue;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new PocketRogue(), config);
 	}
 }
